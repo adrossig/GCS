79e24d2c7cc3c32f6f3543d2eed6ad890d4a0299
adrossig <33868378+adrossig@users.noreply.github.com>
2021-05-13 12:50:02 +0200

    Add files via upload

---
+++ color_screen/client/html/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() 
+{
+	const $bg = $('div');
+
+	GCS.Subscribe('blackscreen/toggle', function(args)
+	{
+		ToggleBlackScreen(args);
+	})
+
+	function ToggleBlackScreen(data)
+	{
+		if (data.time == 0)
+			$bg.css('opacity', data.visible ? '1.0' : '0.0')
+		else
+		{
+			$bg.animate({
+				opacity: data.visible ? 1.0 : 0.0,
+				duration: data.time
+			});
+		}
+	}
+})
